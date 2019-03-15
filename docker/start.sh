0388ac9e3e2086d705ef8978fc2e4450cba8c64b
Marica Antonacci <marica.antonacci@ba.infn.it>
2019-03-15 11:18:17 +0000

    gunicorn command updated

--- docker/start.sh
+++ docker/start.sh
@@ -4,11 +4,11 @@
 
 if [ "${ENABLE_HTTPS}" == "True" ]; then
   if test -e /certs/cert.pem && test -f /certs/key.pem ; then
-    exec gunicorn --bind 0.0.0.0:5001 --certfile /certs/cert.pem --keyfile /certs/key.pem --timeout "$TIMEOUT"  orchdashboard:app
+    exec gunicorn --bind 0.0.0.0:5001 -w "$WORKERS" --certfile /certs/cert.pem --keyfile /certs/key.pem --timeout "$TIMEOUT"  orchdashboard:app
   else
     echo "[ERROR] File /certs/cert.pem or /certs/key.pem NOT FOUND!"
     exit 1
   fi
 else
-  exec gunicorn --bind 0.0.0.0:5001 --timeout "$TIMEOUT"  orchdashboard:app
+  exec gunicorn --bind 0.0.0.0:5001 -w "$WORKERS" --timeout "$TIMEOUT"  orchdashboard:app
 fi
