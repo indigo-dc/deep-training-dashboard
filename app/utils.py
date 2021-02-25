179716f4f172bff2403b1703bdf901a157016490
Ignacio Heredia <iheredia@ifca.unican.es>
2021-02-25 17:55:42 +0100

    add deployment type

--- app/utils.py
+++ app/utils.py
@@ -55,6 +55,26 @@ def loadToscaTemplates(directory):
     return toscaTemplates
 
 
+def getdeploymenttype(nodes):
+    deployment_type = ""
+    for (j, u) in nodes.items():
+        if deployment_type == "":
+            for (k, v) in u.items():
+                if k == "type" and v == "tosca.nodes.indigo.Compute":
+                    deployment_type = "CLOUD"
+                    break
+                if k == "type" and v == "tosca.nodes.indigo.Container.Application.Docker.Marathon":
+                    deployment_type = "MARATHON"
+                    break
+                if k == "type" and v == "tosca.nodes.indigo.Container.Application.Docker.Chronos":
+                    deployment_type = "CHRONOS"
+                    break
+                if k == "type" and v == "tosca.nodes.indigo.Qcg.Job":
+                    deployment_type = "QCG"
+                    break
+    return deployment_type
+
+
 def extractToscaInfo(toscaDir, tosca_pars_dir, toscaTemplates):
     toscaInfo = {}
     for tosca in toscaTemplates:
@@ -85,6 +105,9 @@ def extractToscaInfo(toscaDir, tosca_pars_dir, toscaTemplates):
 
                 if 'inputs' in template['topology_template']:
                     toscaInfo[tosca]['inputs'] = template['topology_template']['inputs']
+
+                if 'node_templates' in template['topology_template']:
+                    toscaInfo[tosca]['deployment_type'] = getdeploymenttype(template['topology_template']['node_templates'])
 
                 # Add parameters code here
                 if tosca_pars_dir:
